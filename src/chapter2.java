eadaf88c775440926334a5a0a72fb4a836d0d231
151250088 <151250088@smail.nju.edu.cn>
2018-01-12 14:26:26 +0800

    在O(1)时间复杂度删除链表节点:不用遍历

--- src/chapter2.java
+++ src/chapter2.java
@@ -341,5 +341,15 @@ public class chapter2 {
        }
     }
 
+    /**
+     * 给定一个单链表中的一个等待被删除的节点(非表头或表尾)。
+     * 请在在O(1)时间复杂度删除该链表节点。
+     * @param node Linked list is 1->2->3->4, and given node 3, delete the node in place 1->2->4
+     */
+       public void deleteNode(ListNode node) {
+           node.val=node.next.val;
+           node.next=node.next.next;
+       }
+
 
 }
