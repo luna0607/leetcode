f0ffba68430eee092eeb97480172a9c7f14d4b5f
151250088 <151250088@smail.nju.edu.cn>
2018-03-05 10:50:13 +0800

    内存优化

--- src/google/MinimumPartition.java
+++ src/google/MinimumPartition.java
@@ -7,15 +7,19 @@ import java.util.Arrays;
  */
 public class MinimumPartition {
        /*link:http://www.lintcode.com/en/problem/minimum-partition/
+       *内存优化。只保留dp[0]和dp[1]两行
      * @param : the given array
      * @return: the minimum difference between their sums
      */
     public int findMin(int[] arr) {
+        if(arr.length==2){
+            return Math.abs(arr[0]-arr[1]);
+        }
         int sum=0;
         for(int i:arr){
             sum+=i;
         }
-        int[][] dp=new int[arr.length][(sum / 2)+1];
+        int[][] dp=new int[2][(sum / 2)+1];
         for(int j=0;j<sum/2+1;j++){
             if(arr[0]<=j){
                 dp[0][j]=arr[0];
@@ -26,16 +30,19 @@ public class MinimumPartition {
         for(int i=1;i<arr.length;i++){
             for(int j=0;j<sum/2+1;j++){
                 if(j>=arr[i]){
-                    dp[i][j]=max(dp[i-1][j],dp[i-1][j-arr[i]]+arr[i]);
+                    dp[1][j]=max(dp[0][j],dp[0][j-arr[i]]+arr[i]);
                 }else {
-                    dp[i][j]=dp[i-1][j];
+                    dp[1][j]=dp[0][j];
                 }
             }
+              for(int tmp=0;tmp<sum/2+1;tmp++){
+                    dp[0][tmp]=dp[1][tmp];
+                }
         }
         int max=0;
         for(int j=0;j<sum/2+1;j++){
-            if(dp[arr.length-1][j]>max){
-                max=dp[arr.length-1][j];
+            if(dp[1][j]>max){
+                max=dp[1][j];
             }
         }
         return Math.abs(sum-max-max);
@@ -51,7 +58,7 @@ public class MinimumPartition {
 
     public static void main(String[] args) {
         MinimumPartition minimumPartition=new MinimumPartition();
-        int[] tmp={1,6,11,5};
+        int[] tmp={616,202,595,876,388,120,238,296};
         System.out.print(minimumPartition.findMin(tmp));
     }
 }
