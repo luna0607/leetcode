9fd87056e0affb6888f0562b45e96beacb0b7ea6
151250088 <151250088@smail.nju.edu.cn>
2018-03-05 19:27:30 +0800

    常规写法找峰值

---
+++ src/google/PeakNumber.java
@@ -0,0 +1,34 @@
+package google;
+
+/**
+ * Created by Ariana on 2018/3/5.
+ */
+public class PeakNumber {
+      /*
+      link:http://www.lintcode.com/en/problem/find-peak-element/
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    public int findPeak(int[] A) {
+        // write your code here
+        if(A.length<3){
+            return -1;
+        }
+        for(int i=1;i<A.length-1;i++){
+            if((A[i]>A[i-1])&&(A[i]>A[i+1])){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] A=new int[100001];
+        for(int i=0;i<100000;i++){
+            A[i]=i;
+        }
+        A[100000]=1;
+        PeakNumber peakNumber=new PeakNumber();
+        System.out.print(peakNumber.findPeak(A));
+    }
+}
